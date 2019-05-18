5ebc9b7cf761c5a0b69597d3d560a6c7bbd39b74
smitco <courtjsmith11@gmail.com>
2019-05-17 17:57:34 -0700

    Lesson 05 pig latinizer

--- main.py
+++ main.py
@@ -16,10 +16,21 @@ def get_fact():
 
     return facts[0].getText()
 
+def get_pig_latin(fact):
+    response = requests.post(
+        'https://hidden-journey-62459.herokuapp.com/piglatinize/', 
+        data={'input_text': fact},
+        allow_redirects=False
+    )
+    
+    return response.headers['Location']
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    body = get_pig_latin(fact)
+    
+    return Response(response=body, mimetype= "text/html")
 
 
 if __name__ == "__main__":
